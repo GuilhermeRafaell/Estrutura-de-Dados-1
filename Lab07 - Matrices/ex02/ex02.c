f8dc9fee83c74bc8e695a934bdaba8b95216663f
Guilherme Rafael <guilerme.cerqueira@ufu.br>
2021-08-02 21:06:25 -0300

    Lab07 nao terminado

--- Lab07 - Matrices/ex02/ex02.c
+++ Lab07 - Matrices/ex02/ex02.c
@@ -3,17 +3,25 @@
 #include <time.h>
 //Valores aleatorios de 0 a 100 foram atribuidos as matrizes
 int main() {
-    int tam = 4;
+    int tam = 3;
     int A[tam][tam],B[tam][tam],C[tam][tam];//matriz A B e C
-    int l,c;
+    int l,c,produto;
 
     srand (time(NULL));
     
     for(l=0; l<tam; l++){
         for(c=0; c<tam; c++){
             A[l][c] = rand() % 100;//sorteia valor para os elem da matriz A.
-            B[l][c] = rand() % 100;//sorteia valor para os elem da matriz B.
-            C[l][c] = A[l][c] + B[l][c];//soma os valores das matrizes A e B e salva em C.       
+            B[l][c] = rand() % 100;//sorteia valor para os elem da matriz B.            
+            produto = 0;
+            for(int i=0; i<tam; i++){
+                produto += A[l][i] * B[i][c];
+                C[l][c] = produto;
+            }
+            
+            //A[l][c] = rand() % 100;//sorteia valor para os elem da matriz A.
+            //B[l][c] = rand() % 100;//sorteia valor para os elem da matriz B.
+            //C[l][c] = A[l][c] + B[l][c];//soma os valores das matrizes A e B e salva em C.       
         }
     }
 
@@ -31,7 +39,7 @@ int main() {
         printf("\n");
     }
 
-    printf("<< Matriz C (soma) >>\n");
+    printf("<< Produto >>\n");
     for(l=0; l<tam; l++){
         for(c=0; c<tam; c++)
             printf("%3d ",C[l][c]);
