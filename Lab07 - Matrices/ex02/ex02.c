d4c51c1d34f1799aab30f8289c6326495fcd7489
Guilherme Rafael <guilerme.cerqueira@ufu.br>
2021-08-22 19:19:08 -0300

    Lab07

--- Lab07 - Matrices/ex02/ex02.c
+++ Lab07 - Matrices/ex02/ex02.c
@@ -1,50 +1,40 @@
-#include <stdio.h> 
-#include <stdlib.h>
-#include <time.h>
-//Valores aleatorios de 0 a 100 foram atribuidos as matrizes
-int main() {
-    int tam = 3;
-    int A[tam][tam],B[tam][tam],C[tam][tam];//matriz A B e C
-    int l,c,produto;
+#include<stdio.h>
+#include<stdlib.h>
 
-    srand (time(NULL));
-    
-    for(l=0; l<tam; l++){
-        for(c=0; c<tam; c++){
-            A[l][c] = rand() % 100;//sorteia valor para os elem da matriz A.
-            B[l][c] = rand() % 100;//sorteia valor para os elem da matriz B.            
-            produto = 0;
-            for(int i=0; i<tam; i++){
-                produto += A[l][i] * B[i][c];
-                C[l][c] = produto;
+
+    int main(){
+        int mat1[3][3], mat2[3][3], mat3[3][3], i, j, k, total=0;
+
+        srand(time(NULL));
+
+        for(i=0; i<3; i++){
+            for(j=0; j<3; j++){
+                mat1[i][j] = (rand()/(double)RAND_MAX)*100;
             }
-            
-            //A[l][c] = rand() % 100;//sorteia valor para os elem da matriz A.
-            //B[l][c] = rand() % 100;//sorteia valor para os elem da matriz B.
-            //C[l][c] = A[l][c] + B[l][c];//soma os valores das matrizes A e B e salva em C.       
         }
-    }
+        for(i=0; i<3; i++){
+            for(j=0; j<3; j++){
+                mat2[i][j] = (rand()/(double)RAND_MAX)*100;
+            }
+        }
+        for(i=0; i<3; i++){
+		    for(j=0; j<3; j++){
+			    mat3[i][j] = 0;
 
-    printf("<< Matriz A >>\n");
-    for(l=0; l<tam; l++){
-        for(c=0; c<tam; c++)
-            printf("%3d ",A[l][c]);
-        printf("\n");
-    }
+			    for(k=0; k<3; k++){
+                    total+=mat1[i][k] * mat2[k][j];
+			    }
 
-    printf("<< Matriz B >>\n");
-    for(l=0; l<tam; l++){
-        for(c=0; c<tam; c++)
-            printf("%3d ",B[l][c]);
-        printf("\n");
+			    mat3[i][j]=total;
+			    total=0;
+		    }
+	    }
+        for(i=0; i<3; i++){
+            for(j=0; j<3; j++){
+                printf("%5d ", mat3[i][j]);
+            }
+            printf("\n");
+        }
+        
+       
     }
-
-    printf("<< Produto >>\n");
-    for(l=0; l<tam; l++){
-        for(c=0; c<tam; c++)
-            printf("%3d ",C[l][c]);
-        printf("\n");
-    }     
-  
-    return 0;
-}
---
+++ Lab07 - Matrices/ex08/ex08.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+    int main(){
+        int mat[6][6], soma[8]={0,0,0,0,0,0,0,0}, i, j, inicioS=1, fimS=5, inicioI=2, fimI=3, somaInf=0;
+
+        /*
+        srand(time(NULL));
+        
+        for(i=0; i<6; i++){
+            for(j=0; j<6; j++){
+                mat[i][j] = (rand()/(double)RAND_MAX)*100;
+            }
+        }
+        */
+        for(i=0; i<6; i++){
+            for(j=0; j<6; j++){
+                scanf("%d", &mat[i][j]);
+
+                if(j>i)
+                    soma[0]+=mat[i][j];
+                if((i+j)<(6-1))
+                    soma[1]+=mat[i][j];
+                if(i>j && i<6-j-1)
+                    soma[3]+=mat[i][j];
+                if(i>j && i<6-j-1 || i<j && i>6-j-1)
+                    soma[4]+=mat[i][j];
+            }
+        }
+
+        for(i=0; i<2; i++){
+            for(j=inicioS; j<=fimS; j++){
+                soma[2]+=mat[i][j];
+            }
+            inicioS++;
+            fimS--;
+        }
+        for(i=4; i<=5; i++){
+            for(j=inicioI; j<=fimI; j++){
+                somaInf+=mat[i][j];
+            }
+            inicioI--;
+            fimI++;
+        }
+        soma[5]=soma[2]+somaInf;
+        soma[6]=soma[3]+soma[2];
+        soma[7]=soma[3]+somaInf;
+
+        for(i=0; i<6; i++){
+            for(j=0; j<6; j++){
+                printf("%3d ", mat[i][j]);
+            }
+            printf("\n");
+        }
+
+        for(i=0; i<8; i++){
+            printf("Soma %d = %d\n", i+1, soma[i]);
+        }
+    }
